4b737e19eb29617bc11a712cbb778dbd40032e25
isaac pugliese <isaac pugliese>
2021-06-17 21:02:26 -0300

    NomeCompletoJS

---
+++ nome.js
@@ -0,0 +1,8 @@
+let nome= prompt("Digite o seu nome");
+
+let sobrenome= prompt("Digite o seu sobrenome");
+
+let nomecompleto= `O Seu Nome Completo é: ${nome} ${sobrenome}.`
+
+alert(nomecompleto);
+
